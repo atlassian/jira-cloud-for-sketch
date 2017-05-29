ba146a7fb73a4da6e865628e9a106f9769fd544e
Tim Pettersen <tpettersen@atlassian.com>
2017-05-29 12:45:29 +1000

    Simple webview

--- src/my-issues.js
+++ src/my-issues.js
@@ -1,3 +1,13 @@
+import WebUI from 'sketch-module-web-view'
+
 export default function (context) {
-  context.document.showMessage('It\'s really alive! 🙌')
+  context.document.showMessage('Launching connect webview')
+  const webUI = new WebUI(context, 'connect.html', {
+    identifier: 'jira-sketch-plugin.connect',
+    height: 280,
+    onlyShowCloseButton: true,
+    hideTitleBar: true,
+    handlers: {}
+  })
+  webUI.eval('window.ready=true')
 }
