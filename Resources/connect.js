32ff1e35df1e3704ac2d4c19dda57ef8aeb3bd53
Tim Pettersen <tpettersen@atlassian.com>
2017-06-27 11:34:54 -0400

    Prettify Resources/connect.js

--- Resources/connect.js
+++ Resources/connect.js
@@ -1,39 +1,45 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import pluginCall from 'sketch-module-web-view/client'
-import AkFieldText from '@atlaskit/field-text';
-import ButtonGroup from '@atlaskit/button-group';
-import Button from '@atlaskit/button';
+import AkFieldText from '@atlaskit/field-text'
+import ButtonGroup from '@atlaskit/button-group'
+import Button from '@atlaskit/button'
 import styled from 'styled-components'
 import '@atlaskit/css-reset'
 
 class Connect extends Component {
   render () {
-    return (     
-     <FormWrapper className='connect'>        
+    return (
+      <FormWrapper className='connect'>
         <p>
-            Enter the URL of your JIRA Cloud instance and hit <b>Connect</b>. 
-            You&apos;ll then be asked to authorize Sketch to act on your behalf.
+          Enter the URL of your JIRA Cloud instance and hit <b>Connect</b>.
+          You'll then be asked to authorize Sketch to act on your behalf.
         </p>
         <AkFieldText
-            ref="jiraUrl"
-            placeholder="sketchfan.atlassian.net"
-            label="JIRA Cloud URL"
-            onChange={(event) => this.setState({jiraUrl: event.target.value})}
-            shouldFitContainer
+          ref='jiraUrl'
+          placeholder='sketchfan.atlassian.net'
+          label='JIRA Cloud URL'
+          onChange={event => this.setState({ jiraUrl: event.target.value })}
+          shouldFitContainer
         />
         <br />
         <ButtonGroup>
-            <Button appearance="primary" type="submit"
-                onClick={() => pluginCall('connectToJira', this.state.jiraUrl)}>Connect</Button>
-            <Button appearance="subtle-link" 
-                onClick={() => pluginCall('cancel')}>Cancel</Button>
-        </ButtonGroup>        
+          <Button
+            appearance='primary'
+            type='submit'
+            onClick={() => pluginCall('connectToJira', this.state.jiraUrl)}
+          >
+            Connect
+          </Button>
+          <Button appearance='subtle-link' onClick={() => pluginCall('cancel')}>
+            Cancel
+          </Button>
+        </ButtonGroup>
       </FormWrapper>
     )
   }
 
-  componentDidMount() {
+  componentDidMount () {
     this.refs.jiraUrl.focus()
   }
 }
@@ -41,6 +47,6 @@ class Connect extends Component {
 const FormWrapper = styled.form`
   min-width: 370px;
   padding: 10px;
-`;
+`
 
 ReactDOM.render(<Connect />, document.getElementById('container'))
