c0face033abfbffcd1e71071ecfcb79f8a99add3
Tim Pettersen <tpettersen@atlassian.com>
2017-06-27 11:33:13 -0400

    Prettify Resources/issues.js

--- Resources/issues.js
+++ Resources/issues.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import pluginCall from 'sketch-module-web-view/client'
 import AkDynamicTable from '@atlaskit/dynamic-table'
-import Lozenge from '@atlaskit/lozenge';
+import Lozenge from '@atlaskit/lozenge'
 import styled from 'styled-components'
 import '@atlaskit/css-reset'
-import { akColorB50 } from '@atlaskit/util-shared-styles';
+import { akColorB50 } from '@atlaskit/util-shared-styles'
 
 const TableWrapper = styled.div`
   min-width: 550px;
   padding: 10px;
-`;
+`
 
 const KeyWrapper = styled.span`
   display: flex;
   align-items: center;
   cursor: pointer;
-`;
+`
 
 const Icon = styled.img`
   border-radius: 3px;
   display: inline-block;
   vertical-align: middle;
   margin-right: 8px;
-`;
+`
 
 const head = {
   cells: [
@@ -31,96 +31,99 @@ const head = {
       key: 'issueKey',
       content: 'Key',
       isSortable: true,
-      width: 15,
+      width: 15
     },
     {
       key: 'summary',
       content: 'Summary',
       shouldTruncate: true,
       isSortable: true,
-      width: 50,
+      width: 50
     },
     {
       key: 'status',
       content: 'Status',
       shouldTruncate: true,
       isSortable: true,
-      width: 20,
+      width: 20
     }
   ]
-};
+}
 
-const statusCategoryToAppearance = function(statusCategory) {
+const statusCategoryToAppearance = function (statusCategory) {
   switch (statusCategory) {
-    case "new":
-      return "new"
-    case "indeterminate":
-      return "inprogress"
-    case "done":
-      return "success"
-    case "undefined":
+    case 'new':
+      return 'new'
+    case 'indeterminate':
+      return 'inprogress'
+    case 'done':
+      return 'success'
+    case 'undefined':
     default:
-      return "default"
+      return 'default'
   }
 }
 
-function browseUrl(issue) {
-  return issue.self.substring(0, issue.self.indexOf('/rest/')) + '/browse/' + issue.key
+function browseUrl (issue) {
+  var baseUrl = issue.self.substring(0, issue.self.indexOf('/rest/'))
+  return `${baseUrl}/browse/${issue.key}`
 }
 
 class IssueSummary extends Component {
-  constructor(props) {
-    super(props);
-    this.dragEnter = this.dragEnter.bind(this);
-    this.dragLeave = this.dragLeave.bind(this);
-    this.dragStart = this.dragStart.bind(this);
-    this.drop = this.drop.bind(this);
+  constructor (props) {
+    super(props)
+    this.dragEnter = this.dragEnter.bind(this)
+    this.dragLeave = this.dragLeave.bind(this)
+    this.dragStart = this.dragStart.bind(this)
+    this.drop = this.drop.bind(this)
     this.state = {
       dragHover: false
     }
   }
-  dragEnter(event) {
-    this.setState({dragHover: true});
+  dragEnter (event) {
+    this.setState({ dragHover: true })
     // event.dataTransfer.dropEffect = "copy";
   }
-  dragLeave(event) {
-    this.setState({dragHover: false});
+  dragLeave (event) {
+    this.setState({ dragHover: false })
   }
-  dragStart(event) {
+  dragStart (event) {
     // event.dataTransfer.effectAllowed = "copy";
   }
-  drop(event) {    
-    var files = event.dataTransfer.files;
+  drop (event) {
+    var files = event.dataTransfer.files
     for (var i = 0; i < files.length; i++) {
-        console.log(" File " + i + ":\n(" + (typeof files[i]) + ") : <" + files[i] + " > " +
-          files[i].name + " " + files[i].size + "\n");
+      console.log(`File ${i}: ${files[i].name} ${files[i].size}`)
     }
-    this.setState({dragHover: false});
-    event.preventDefault();
+    this.setState({ dragHover: false })
+    event.preventDefault()
   }
-  preventDefault(event) {
-    event.preventDefault();
+  preventDefault (event) {
+    event.preventDefault()
   }
-  render(props) {
+  render (props) {
     var style = {}
     if (this.state.dragHover) {
-      style.backgroundColor = akColorB50;
+      style.backgroundColor = akColorB50
     }
     return (
-      <div 
+      <div
         draggable='true'
-        style={style} 
-        onDragEnter={this.dragEnter} 
+        style={style}
+        onDragEnter={this.dragEnter}
         onDragLeave={this.dragLeave}
         onDragStart={this.dragStart}
         onDragOver={this.preventDefault}
-        onDropCapture={this.drop}>{this.props.summary}</div>
+        onDropCapture={this.drop}
+      >
+        {this.props.summary}
+      </div>
     )
   }
 }
 
-const rows = function(issues) {
-  issues = issues || [];  
+const rows = function (issues) {
+  issues = issues || []
   return issues.map(issue => ({
     cells: [
       {
@@ -128,39 +131,45 @@ const rows = function(issues) {
         content: (
           <KeyWrapper>
             <Icon src={issue.fields.issuetype.iconUrl} />
-            <span onClick={() => pluginCall('openInBrowser', browseUrl(issue))}>{issue.key}</span>
+            <span onClick={() => pluginCall('openInBrowser', browseUrl(issue))}>
+              {issue.key}
+            </span>
           </KeyWrapper>
-        ),
+        )
       },
       {
         key: issue.fields.summary,
-        content: (
-          <IssueSummary summary={issue.fields.summary} />
-        )
+        content: <IssueSummary summary={issue.fields.summary} />
       },
       {
         key: issue.fields.status.name,
         content: (
-          <Lozenge appearance={statusCategoryToAppearance(issue.fields.status.statusCategory.key)}>{issue.fields.status.name}</Lozenge>
+          <Lozenge
+            appearance={statusCategoryToAppearance(
+              issue.fields.status.statusCategory.key
+            )}
+          >
+            {issue.fields.status.name}
+          </Lozenge>
         )
-      },
-    ],
+      }
+    ]
   }))
-};
+}
 
 class IssueTable extends Component {
   constructor (props) {
-    super(props)    
+    super(props)
     this.state = {
-      issues: (window.issues || []),
+      issues: window.issues || [],
       ready: window.ready
     }
     if (!window.ready) {
       const interval = setInterval(() => {
         if (window.ready) {
           this.setState({
-            issues: (window.issues || []),
-            ready: window.ready            
+            issues: window.issues || [],
+            ready: window.ready
           })
           clearInterval(interval)
         }
@@ -168,8 +177,8 @@ class IssueTable extends Component {
     }
   }
 
-  render (props) {    
-    return (      
+  render (props) {
+    return (
       <TableWrapper>
         {!this.state.ready && 'loading...'}
         <AkDynamicTable
