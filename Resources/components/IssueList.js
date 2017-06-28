41241a72d626e8ee86141a6c8c89a8ed31fd1310
Tim Pettersen <tpettersen@atlassian.com>
2017-06-28 10:20:16 -0400

    Issue view info bar

--- Resources/components/IssueList.js
+++ Resources/components/IssueList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import Avatar from '@atlaskit/avatar'
+import AssigneeAvatar from './AssigneeAvatar'
 import styled from 'styled-components'
 import '@atlaskit/css-reset'
 
@@ -52,7 +52,7 @@ class Issue extends Component {
         <IssueTypeField type={issue.fields.issuetype} />
         <IssueKeyField issueKey={issue.key} />
         <IssueSummaryField summary={issue.fields.summary} />
-        <IssueAssigneeField assignee={issue.fields.assignee} />
+        <AssigneeAvatar assignee={issue.fields.assignee} />
       </IssueDiv>
     )
   }
@@ -149,53 +149,3 @@ const SummaryDiv = styled.div`
   width: 280px;
 `
 
-class IssueAssigneeField extends Component {
-  render () {
-    var assignee = this.props.assignee
-    var avatarUrl, title
-    if (assignee) {
-      avatarUrl = assignee.avatarUrls['24x24']
-      title = `Assigned to ${assignee.displayName || assignee.name}`
-    } else {
-      title = 'Unassigned'
-    }
-    return (
-      <AvatarDiv>
-        <AvatarWrapper title={title}>
-          <Avatar src={avatarUrl} size='small' label={title} />
-        </AvatarWrapper>
-      </AvatarDiv>
-    )
-  }
-}
-
-IssueAssigneeField.propTypes = {
-  /*
-  {
-    "self": "https://jira.example.com/rest/api/2/user?username=admin",
-    "name": "admin",
-    "key": "admin",
-    "accountId": "xxxxxxxx",
-    "emailAddress": "example@example.com",
-    "avatarUrls": {
-      "48x48": "https://...",
-      "24x24": "https://...",
-      "16x16": "https://...",
-      "32x32": "https://..."
-    },
-    "displayName": "Tim Pettersen",
-    "active": true,
-    "timeZone": "Australia/Sydney"
-  }
-  */
-  assignee: PropTypes.object
-}
-
-const AvatarDiv = styled.div`
-  width: 30px;
-`
-
-const AvatarWrapper = styled.div`
-  margin-top: 5px;
-  margin-right: 5px;
-`
--- Resources/components/IssueView.js
+++ Resources/components/IssueView.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import pluginCall from 'sketch-module-web-view/client'
+import AssigneeAvatar from './AssigneeAvatar'
 import styled from 'styled-components'
 import '@atlaskit/css-reset'
 
@@ -38,9 +40,11 @@ const ClickableH3 = styled.h3`
 
 class IssueSummary extends Component {
   render () {
+    var issue = this.props.issue
     return (
       <SummaryDiv>
-        <div>{this.props.issue.fields.summary}</div>
+        <SummaryText>{issue.fields.summary}</SummaryText>
+        <SummaryInfoBar issue={issue} />
       </SummaryDiv>
     )
   }
@@ -52,10 +56,85 @@ IssueSummary.propTypes = {
 
 const SummaryDiv = styled.div`
   margin-top: 10px;
-  padding: 10px;
-  height: 100px;
+  padding: 12px 12px 3px 12px;
   border-radius: 3px;
   background: white;
+`
+
+const SummaryText = styled.div`
+  max-height: 70px;
+`
+
+class SummaryInfoBar extends Component {
+  render () {
+    var issue = this.props.issue
+    return (
+      <InfoBarDiv>
+        <IssueType type={issue.fields.issuetype} />
+        <KeyAndAvatarWrapper>
+          <IssueKey issue={issue} />
+          <AssigneeAvatar assignee={issue.fields.assignee} />
+        </KeyAndAvatarWrapper>
+      </InfoBarDiv>
+    )
+  }
+}
+
+SummaryInfoBar.propTypes = {
+  issue: PropTypes.object.isRequired
+}
+
+const InfoBarDiv = styled.div`
+  margin-top: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const KeyAndAvatarWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+class IssueType extends Component {
+  render () {
+    var type = this.props.type
+    return (
+      <img src={type.iconUrl} title={type.name} />
+    )
+  }
+}
+
+IssueType.propTypes = {
+  type: PropTypes.object.isRequired
+}
+
+class IssueKey extends Component {
+  constructor (props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  render () {
+    return (
+      <IssueKeyLink onClick={this.handleClick}>{this.props.issue.key}</IssueKeyLink>
+    )
+  }
+  handleClick () {
+    pluginCall('openInBrowser', this.browseUrl())
+  }
+  browseUrl () {
+    var issue = this.props.issue
+    var baseUrl = issue.self.substring(0, issue.self.indexOf('/rest/'))
+    return `${baseUrl}/browse/${issue.key}`
+  }
+}
+
+IssueKey.propTypes = {
+  issue: PropTypes.object.isRequired
+}
+
+const IssueKeyLink = styled.a`
+  margin-right: 10px;
 `
 
 class DropZone extends Component {
