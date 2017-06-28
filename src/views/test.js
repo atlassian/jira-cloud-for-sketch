790059f55c1b87d418e537f369278b7ae18009a6
Tim Pettersen <tpettersen@atlassian.com>
2017-06-28 15:59:12 -0400

    Remove unused drop test

--- src/views/test.js
+++ src/views/test.js
@@ -1,21 +1,8 @@
 import '../defaultImports'
-import WebUI from 'sketch-module-web-view'
-import { executeSafelyAsync } from '../util'
-import DragUIDelegate from '../dragndrop-uidelegate'
+import { executeSafely } from '../util'
 
 export default function (context) {
-  executeSafelyAsync(context, async function () {
-    const webUI = new WebUI(context, 'drop.html', {
-      identifier: 'jira-sketch-plugin.drop',
-      height: 200,
-      width: 200,
-      onlyShowCloseButton: true,
-      hideTitleBar: false,
-      title: 'DropZone',
-      handlers: {}
-    })
-    var uiDelegate = DragUIDelegate(context, console.log)
-    webUI.webView.setUIDelegate_(uiDelegate.new())
-    webUI.eval('window.ready=true')
+  executeSafely(context, function () {
+    context.document.showMessage(`Test action`)
   })
 }
