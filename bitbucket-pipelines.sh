f064f6409c969b82fadd0876045f708c29da5611
Tim Pettersen <tpettersen@atlassian.com>
2017-07-15 14:16:35 -0700

    Publish link to plugin artifact as build status

--- bitbucket-pipelines.sh
+++ bitbucket-pipelines.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
+
+# install deps (TODO move these into a Dockerfile)
+apt-get update && apt-get install -y zip python-pip python-dev
+pip install boto3
+npm install -g skpm@0.9.16
+
+# run the tests
 npm install
 npm test
-npm install -g skpm@0.9.16
+
+# build the plugin
 skpm build
-apt-get update && apt-get install -y zip python-pip python-dev
-zip -r "jira.sketchplugin-$BITBUCKET_COMMIT.zip" jira.sketchplugin
-pip install boto3
-python s3_upload.py atlassian-sketch-plugin "jira.sketchplugin-$BITBUCKET_COMMIT.zip" "jira.sketchplugin-$BITBUCKET_COMMIT.zip"
+export PLUGIN_ZIP="jira.sketchplugin-${BITBUCKET_COMMIT}.zip"
+zip -r $PLUGIN_ZIP jira.sketchplugin
+
+# deploy plugin artifact(s)
+python s3_upload.py atlassian-sketch-plugin $PLUGIN_ZIP $PLUGIN_ZIP
 if [ -v PIPELINES_DEPLOY_AS_LATEST ]; then
   python s3_upload.py atlassian-sketch-plugin "jira.sketchplugin-$BITBUCKET_COMMIT.zip" "jira.sketchplugin-latest.zip"
 fi
+
+# publish build status
+export S3_URL="https://s3-us-west-2.amazonaws.com/atlassian-sketch-plugin/${PLUGIN_ZIP}"
+export BUILD_STATUS="{\"key\": \"sketch-plugin-zip\", \"state\": \"SUCCESSFUL\", \"name\": \"Sketch Plugin ZIP\", \"url\": \"${S3_URL}\"}"
+curl -H "Content-Type: application/json" -X POST --user "${BB_UPLOAD_USERNAME}:${BB_UPLOAD_PASSWORD}" -d "${BUILD_STATUS}" "https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/commit/${BITBUCKET_COMMIT}/statuses/build"
