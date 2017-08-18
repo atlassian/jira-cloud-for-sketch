907594c5be41c0199bede7203cf5d2f66d97e0a6
Tim Pettersen <tpettersen@atlassian.com>
2017-08-18 14:28:27 +1000

    Rebrand as Atlassian Sketch Plugin

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# JIRA Sketch Plugin
+# Atlassian Sketch Plugin
 
 A Sketch plugin that provides integration with JIRA, via a companion Atlassian Connect add-on ([jira-sketch-integration](https://bitbucket.org/atlassian/jira-sketch-integration))
 
--- bitbucket-pipelines.sh
+++ bitbucket-pipelines.sh
@@ -16,24 +16,27 @@ npm test
 
 # build the plugin
 skpm build
-export PLUGIN_ZIP="jira.sketchplugin-${BITBUCKET_COMMIT}.zip"
-zip -r $PLUGIN_ZIP jira.sketchplugin
+export PLUGIN_NAME="atlassian.sketchplugin"
+export PLUGIN_ZIP="${PLUGIN_NAME}-${BITBUCKET_COMMIT}.zip"
+export S3_BUCKET="atlassian-sketch-plugin"
+zip -r $PLUGIN_ZIP $PLUGIN_NAME
 
 # deploy plugin artifact(s)
-python s3_upload.py atlassian-sketch-plugin $PLUGIN_ZIP $PLUGIN_ZIP
+python s3_upload.py $S3_BUCKET $PLUGIN_ZIP $PLUGIN_ZIP
 if [ -v PIPELINES_DEPLOY_AS_LATEST ]; then
-  python s3_upload.py atlassian-sketch-plugin "jira.sketchplugin-$BITBUCKET_COMMIT.zip" "jira.sketchplugin-latest.zip"
+  python s3_upload.py $S3_BUCKET $PLUGIN_ZIP "${PLUGIN_NAME}-latest.zip"
 fi
 if [ -v PIPELINES_DEPLOY_TAG ]; then
-  python s3_upload.py atlassian-sketch-plugin "jira.sketchplugin-$BITBUCKET_COMMIT.zip" "jira.sketchplugin-$BITBUCKET_TAG.zip"
+  python s3_upload.py $S3_BUCKET $PLUGIN_ZIP "${PLUGIN_NAME}-$BITBUCKET_TAG.zip"
+  python s3_upload.py $S3_BUCKET $PLUGIN_ZIP "${PLUGIN_NAME}-release.zip"
 fi
 
 # deploy appcast.xml
 if [ -v PIPELINES_UPDATE_APPCAST ]; then
-  python s3_upload.py atlassian-sketch-plugin appcast.xml appcast.xml
+  python s3_upload.py $S3_BUCKET appcast.xml appcast.xml
 fi
 
 # publish build status
-export S3_URL="https://s3-us-west-2.amazonaws.com/atlassian-sketch-plugin/${PLUGIN_ZIP}"
+export S3_URL="https://s3-us-west-2.amazonaws.com/${S3_BUCKET}/${PLUGIN_ZIP}"
 export BUILD_STATUS="{\"key\": \"sketch-plugin-zip\", \"state\": \"SUCCESSFUL\", \"name\": \"Sketch Plugin ZIP\", \"url\": \"${S3_URL}\"}"
 curl -H "Content-Type: application/json" -X POST --user "${BB_UPLOAD_USERNAME}:${BB_UPLOAD_PASSWORD}" -d "${BUILD_STATUS}" "https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/commit/${BITBUCKET_COMMIT}/statuses/build"
