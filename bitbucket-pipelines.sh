41279a440261ffa1a11898f11cfa49e58d144c68
Tim Pettersen <tpettersen@atlassian.com>
2017-09-14 10:45:14 +1000

    ASP-67: automate publishing to GitHub

--- bitbucket-pipelines.sh
+++ bitbucket-pipelines.sh
@@ -19,13 +19,15 @@ export PLUGIN_ZIP="${PLUGIN_NAME}-${BITBUCKET_COMMIT}.zip"
 export S3_BUCKET="atlassian-sketch-plugin"
 export S3_BUCKET_URL="https://s3-us-west-2.amazonaws.com/${S3_BUCKET}"
 
+# get access token for posting build statuses and pushing to Bitbucket
+export BITBUCKET_ACCESS_TOKEN=`curl -s -X POST -u "${BB_CONSUMER_KEY}:${BB_CONSUMER_SECRET}" https://bitbucket.org/site/oauth2/access_token -d grant_type=client_credentials | jq .access_token -r`
+
 zip -r $PLUGIN_ZIP $PLUGIN_NAME
 
 # postBuildStatus $key $name $description $url
 postBuildStatus () {
   node_modules/.bin/bbuild \
-    -u ${BB_UPLOAD_USERNAME} \
-    -p ${BB_UPLOAD_PASSWORD} \
+    -b ${BITBUCKET_ACCESS_TOKEN} \
     -o ${BITBUCKET_REPO_OWNER} \
     -r ${BITBUCKET_REPO_SLUG} \
     -c ${BITBUCKET_COMMIT} \
@@ -48,9 +50,24 @@ if [ -v PIPELINES_DEPLOY_TAG ]; then
   postBuildStatus "sketch-plugin-release-zip" $PLUGIN_RELEASE_ZIP \
     "Sketch plugin release zip" "${S3_BUCKET_URL}/${PLUGIN_RELEASE_ZIP}"
   python s3_upload.py $S3_BUCKET $PLUGIN_ZIP "${PLUGIN_NAME}.zip"
-fi
-
-# deploy appcast.xml
-if [ -v PIPELINES_UPDATE_APPCAST ]; then
   python s3_upload.py $S3_BUCKET appcast.xml appcast.xml
+
+  # update settings for write access
+  git remote set-url origin https://x-token-auth:${BITBUCKET_ACCESS_TOKEN}@bitbucket.org/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}.git
+  git config user.name "${COMMITTER_NAME}"
+  git config user.email "${COMMITTER_EMAIL}"
+
+  # switch to release branch without touching index / working copy
+  git fetch origin $BITBUCKET_RELEASE_BRANCH:$BITBUCKET_RELEASE_BRANCH
+  git reset $BITBUCKET_RELEASE_BRANCH
+  git checkout $BITBUCKET_RELEASE_BRANCH
+
+  # commit source from tag plus compiled plugin files
+  git add .
+  git add -f $PLUGIN_NAME
+  git commit -m "Build v$BITBUCKET_TAG"
+
+  # push to Bitbucket & GitHub
+  git push origin $BITBUCKET_RELEASE_BRANCH
+  git push -f $GITHUB_SSH_URL $BITBUCKET_RELEASE_BRANCH:$GITHUB_RELEASE_BRANCH
 fi
